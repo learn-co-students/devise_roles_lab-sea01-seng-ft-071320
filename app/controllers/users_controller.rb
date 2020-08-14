437865bf94852400861c2b9af3b0f9d64d5f6c98
afteralec <github email address>
2020-08-13 17:27:22 -0700

    Introduced working user and post models and users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  before_action :draw_user
+  before_action :authorize_admin, only: [ :destroy ]
+
+  def destroy
+    @user.destroy
+  end
+
+  private
+
+  def authorize_admin
+    return head(:unauthorized) unless current_user.admin?
+  end
+
+  def draw_user
+    @user = User.find(params[:id])
+  end
 end
